287fb67ec94312f9afca2da37466c34fd3c2714f
Anne Pagno <a.pagno@gmail.com>
2016-01-18 15:05:43 -0200

    plataformas e animações do char com alguns bugs

--- Assets/Scripts/Controls/Player.cs
+++ Assets/Scripts/Controls/Player.cs
@@ -92,9 +92,9 @@ public class Player : MonoBehaviour {
             velocity.y += gravity * Time.deltaTime;
             heroPhysics.Move(velocity * Time.deltaTime, inputAxes);
 
-            //heroAnimator.SetFloat ("horizontalSpeed", Mathf.Abs(heroPhysics.currentVelocity.x / Time.deltaTime));
-            //heroAnimator.SetFloat ("verticalSpeed", heroPhysics.currentVelocity.y / Time.deltaTime);
-            //heroAnimator.SetBool ("onGround", heroPhysics.collisions.below);
+            heroAnimator.SetFloat ("horizontalSpeed", Mathf.Abs(heroPhysics.currentVelocity.x / Time.deltaTime));
+            heroAnimator.SetFloat ("verticalSpeed", heroPhysics.currentVelocity.y / Time.deltaTime);
+            heroAnimator.SetBool ("onGround", heroPhysics.collisions.below);
         }
         if (invincible) {
             invincibleTimer -= Time.deltaTime;
