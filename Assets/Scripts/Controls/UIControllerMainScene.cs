37ec61653e4ac99c7976dc021d82ffe8f385f0aa
Bruno Pagno <brunopagno@gmail.com>
2016-02-07 12:51:58 -0200

    Added UI for the inventory

---
+++ Assets/Scripts/Controls/UIControllerMainScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControllerMainScene : MonoBehaviour {
+
+    public GameObject office;
+    public GameObject bedroom;
+
+    private Player player;
+    private const string OFFICE = "office";
+    private const string BEDROOM = "bedroom";
+
+    void Start () {
+        this.player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+
+    void Update() {
+        foreach (Collectible item in player.inventory.items) {
+            if (item.identifier == OFFICE) {
+                office.SetActive(true);
+            } else if (item.identifier == BEDROOM) {
+                bedroom.SetActive(true);
+            }
+        }
+    }
+
+}
